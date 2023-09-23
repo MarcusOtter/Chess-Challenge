8ab5d9f4038c5050af9edbe0b267bc5547a3397d
MarcusOtter <github@otterstrom.dev>
2023-09-23 17:22:58 +0200

    Experimenting with tokens

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,10 +1,35 @@
-﻿using ChessChallenge.API;
+﻿using System;
+using ChessChallenge.API;
 
 public class MyBot : IChessBot
 {
+    
+    private double[] largeDoubles = {
+        1234567890123456789012345678901234567890123d,
+        0x0000000000000000,
+    };
+
+    private ulong largeNumber = 12345678901234567890;
+    
+    public MyBot()
+    {
+        Console.WriteLine(largeDoubles[0].ToString("0.0"));
+        Console.WriteLine(largeNumber.ToString("0.0"));
+    }
+    
     public Move Think(Board board, Timer timer)
     {
-        Move[] moves = board.GetLegalMoves();
-        return moves[0];
+        var moves = board.GetLegalMoves();
+        foreach (var move in moves)
+        {
+            if (move.IsCapture && !board.SquareIsAttackedByOpponent(move.TargetSquare))
+            {
+                return move;
+            }
+        }
+
+        // var queenMoves = moves.Where(x => x.MovePieceType == PieceType.Queen);
+        // var captureMove = moves.FirstOrDefault(x => x.IsCapture);
+        return moves[new Random().Next(0, moves.Length)];
     }
 }
